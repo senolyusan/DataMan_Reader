d355ba7c75c01cedd386500a80028bb393da99bc
cuijiange <yuyongyang977@hotmail.com>
2021-04-23 10:20:56 +0800

    添加定时器，保持发现能力

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -30,32 +30,43 @@ namespace WindowsFormsApp2
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pic_NG = new System.Windows.Forms.PictureBox();
+            this.pic_OK = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_NG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_OK)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
+            this.comboBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 24);
+            this.comboBox1.Location = new System.Drawing.Point(104, 17);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.Size = new System.Drawing.Size(229, 24);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(10, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.Size = new System.Drawing.Size(88, 16);
             this.label1.TabIndex = 3;
-            this.label1.Text = "端口号：";
+            this.label1.Text = "选择扫码枪";
             // 
             // dataGridView1
             // 
@@ -66,11 +77,12 @@ namespace WindowsFormsApp2
             this.dataGridView1.Location = new System.Drawing.Point(12, 51);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(347, 387);
+            this.dataGridView1.Size = new System.Drawing.Size(427, 387);
             this.dataGridView1.TabIndex = 6;
             // 
             // Column1
             // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.Column1.HeaderText = "扫码内容";
             this.Column1.Name = "Column1";
             // 
@@ -78,19 +90,77 @@ namespace WindowsFormsApp2
             // 
             this.Column2.HeaderText = "扫码时间";
             this.Column2.Name = "Column2";
+            this.Column2.Width = 152;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(445, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(322, 30);
+            this.textBox1.TabIndex = 7;
+            // 
+            // pic_NG
+            // 
+            this.pic_NG.Image = ((System.Drawing.Image)(resources.GetObject("pic_NG.Image")));
+            this.pic_NG.Location = new System.Drawing.Point(454, 116);
+            this.pic_NG.Name = "pic_NG";
+            this.pic_NG.Size = new System.Drawing.Size(302, 308);
+            this.pic_NG.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic_NG.TabIndex = 8;
+            this.pic_NG.TabStop = false;
+            // 
+            // pic_OK
+            // 
+            this.pic_OK.Image = ((System.Drawing.Image)(resources.GetObject("pic_OK.Image")));
+            this.pic_OK.Location = new System.Drawing.Point(454, 116);
+            this.pic_OK.Name = "pic_OK";
+            this.pic_OK.Size = new System.Drawing.Size(302, 308);
+            this.pic_OK.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic_OK.TabIndex = 9;
+            this.pic_OK.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(339, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "打开存档";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(445, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 29);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "存储文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pic_NG);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pic_OK);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_NG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_OK)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,6 +174,12 @@ namespace WindowsFormsApp2
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pic_NG;
+        private System.Windows.Forms.PictureBox pic_OK;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
 using Ports = System.IO.Ports;
 using SerialPort = System.IO.Ports.SerialPort;
 using Cognex.DataMan.SDK;
@@ -14,65 +15,301 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// 扫描枪信息集合
+        /// </summary>
+        List<SerSystemDiscoverer.SystemInfo> systemInfos = new List<SerSystemDiscoverer.SystemInfo>();
+        /// <summary>
+        /// 获取扫描枪委托
+        /// </summary>
+        /// <param name="obj">扫描枪信息</param>
+        public delegate void _getScanner(object obj);
+        /// <summary>
+        /// 获取扫描结果委托
+        /// </summary>
+        /// <param name="obj"></param>
+        public delegate void _getData(String str);
+        /// <summary>
+        /// 设置文本委托
+        /// </summary>
+        /// <param name="text"></param>
+        public delegate void _setText(String text);
+
+        public delegate void _displayAlert(Boolean flag);
+        private  _getScanner GetScanner;
+        private _getData GetData;
+        private _setText SetText;
+        private _displayAlert DisplayAlert;
+        public Dictionary<string, string> ScanData = new Dictionary<string, string>();
+        SerSystemConnector serSystemConnector = null;
+        DataManSystem dataManSystem = null;
+        private string storeFile = null;
+        public delegate void FreshScannerList();
+        public event FreshScannerList FreshEvent;
+        public delegate void _discoverScanner();
+        private _discoverScanner DiscoverScanner;
+        SerSystemDiscoverer serSystemDiscoverer =null;
+        protected virtual void OnFreshScannerList()
+        {
+            FreshScannerList eventHandler = FreshEvent;
+            if (eventHandler != null)
+            {
+                eventHandler();
+            }
+        }
         public Form1()
         {
             InitializeComponent();
-            GetScanner = new _getScanner(addListItem);
+            GetScanner = new _getScanner(ComBoBoxaddListItem);
             GetData = new _getData(DataGridAddRows);
+            SetText = new _setText(TextBoxSetText);
+            DisplayAlert = new _displayAlert(displayAlert);
+            DiscoverScanner = new _discoverScanner(discoverScanner);
         }
-        List<SerSystemDiscoverer.SystemInfo> systemInfos = new List<SerSystemDiscoverer.SystemInfo>();
-        public delegate void _getScanner(object obj);
-        public delegate void _getData(object obj);
-        public _getScanner GetScanner;
-        public _getData GetData;
+        private void displayAlert(Boolean flag)
+        {
+            if (flag) 
+            { 
+                this.pic_NG.Visible = true;
+                this.pic_OK.Visible = false;                 
+                dataManSystem.SendCommand("OUTPUT.DATAVALID-FAIL");
+            }
+            else
+            {                
+                this.pic_NG.Visible = false;
+                this.pic_OK.Visible = true;
+            }
+
+        }
         private void Form1_Load(object sender, EventArgs e)
-        {            
-            SerSystemDiscoverer serSystemDiscoverer = new SerSystemDiscoverer();
+        {
+            this.pic_NG.Visible = false;
+            this.pic_OK.Visible = false;
+            this.textBox1.Enabled = false;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.RowHeadersVisible = false;
+            serSystemDiscoverer = new SerSystemDiscoverer();
             serSystemDiscoverer.SystemDiscovered += SerSystemDiscoverer_SystemDiscovered;
-            serSystemDiscoverer.Discover();
-            
-            
+            timer1.Interval = 1000;
+            timer1.Tick += Timer1_Tick;
+            timer1.Enabled = true;
+            timer1.Start();
+
         }
-        public void addListItem(object obj)
+        private void discoverScanner()
+        {
+            if(dataManSystem == null && serSystemDiscoverer !=null)
+            {
+                serSystemDiscoverer.Discover();
+            }
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (dataManSystem == null)
+            {
+                this.Invoke(DiscoverScanner);
+            }
+        }
+
+        /// <summary>
+        /// 扫描枪下拉列表
+        /// </summary>
+        /// <param name="obj"></param>
+        public void ComBoBoxaddListItem(object obj)
         {
             
             comboBox1.Items.Add(obj);
         }
-
-        public void DataGridAddRows(object obj)
+        /// <summary>
+        /// 接收结果视图更新
+        /// </summary>
+        /// <param name="str"></param>
+        public void DataGridAddRows(string str)
         {
+            this.Invoke(SetText, str);
+            string timeStr = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+            if (ScanData.ContainsKey(str))
+            {                
+                this.Invoke(DisplayAlert,true);
+            }
+            else
+            {
+                this.Invoke(DisplayAlert, false);
+                ScanData.Add(str, timeStr);
+                dataGridView1.Rows.Insert(0, str, timeStr);
+                str.Replace("\"", "\"\"");
+                if(str.Contains(",") || str.Contains("\r") || str.Contains("\n") || str.Contains("\t") || str.Contains(" ") || str.Contains("\""))
+                {
+                    str =$"\"{str}\"";
+                }
+                if (storeFile == null)
+                {
 
-            dataGridView1.Rows.Add(obj,DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"));
+                    SaveFileDialog openFile = new SaveFileDialog();
+                    openFile.Filter = "CSV文件|*.csv";
+                    openFile.FilterIndex = 0;
+                    if (DialogResult.OK == openFile.ShowDialog())
+                    {
+                        storeFile = openFile.FileName;
+                    }
+                    else
+                    {
+                        MessageBox.Show("必须指定存储文件");
+                        return;
+                    }
+                }
+                System.IO.FileStream fs = new System.IO.FileStream(storeFile, System.IO.FileMode.Append, System.IO.FileAccess.Write);
+                System.IO.StreamWriter streamWriter = new System.IO.StreamWriter(fs, Encoding.UTF8);
+                
+                streamWriter.WriteLine($"{str}\t{timeStr}");
+                streamWriter.Close();
+                fs.Close();
+            }
+            
         }
+        public void TextBoxSetText(string text)
+        {
+            this.textBox1.Text = text;
+        }
+        /// <summary>
+        /// 扫码枪发现事件处理函数
+        /// </summary>
+        /// <param name="systemInfo"></param>
         private void SerSystemDiscoverer_SystemDiscovered(SerSystemDiscoverer.SystemInfo systemInfo)
         {
             systemInfos.Add(systemInfo);
             this.Invoke(GetScanner, systemInfo);
         }
-
-        private void cOMToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
+        /// <summary>
+        /// 扫码枪列表框选择项更改
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            SerSystemConnector serSystemConnector = new SerSystemConnector(comboBox1.SelectedItem.ToString());
-            DataManSystem dataManSystem = new DataManSystem(serSystemConnector);
-            try {
-                dataManSystem.ReadStringArrived -= DataManSystem_ReadStringArrived; 
+            if(serSystemConnector != null || dataManSystem != null)
+            { 
+                if(dataManSystem == null)
+                {
+                    serSystemConnector.Disconnect();
+                    serSystemConnector.Dispose();
+                    serSystemConnector = null;
+                }
+                else if(serSystemConnector == null)
+                {
+                    dataManSystem.Disconnect();
+                    dataManSystem.Dispose();
+                    dataManSystem = null;
+                }
+                else
+                {
+                    try {
+                        dataManSystem.ReadStringArrived -= DataManSystem_ReadStringArrived;
+                    }
+                    catch
+                    {
+
+                    }
+                    dataManSystem.Disconnect();
+                    dataManSystem.Dispose();
+                    dataManSystem = null;
+                    serSystemConnector.Disconnect();
+                    serSystemConnector.Dispose();
+                    serSystemConnector = null;
+                }
             }
-            finally { 
-                dataManSystem.ReadStringArrived += DataManSystem_ReadStringArrived;
-                dataManSystem.Connect();
+            try
+            {
+                serSystemConnector = new SerSystemConnector(comboBox1.SelectedItem.ToString());
+                dataManSystem = new DataManSystem(serSystemConnector);
             }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            try 
+            {
+                dataManSystem.ReadStringArrived -= DataManSystem_ReadStringArrived;                
+            } finally 
+            { 
+                dataManSystem.ReadStringArrived += DataManSystem_ReadStringArrived;                
+            }
+            try
+            {
+                dataManSystem.SystemDisconnected -= DataManSystem_SystemDisconnected;
+            }finally
+            {
+                dataManSystem.SystemDisconnected += DataManSystem_SystemDisconnected;
+            }
+            dataManSystem.Connect();
             
+        }
+
+        private void DataManSystem_SystemDisconnected(object sender, EventArgs args)
+        {
+            this.OnFreshScannerList();
         }
 
         private void DataManSystem_ReadStringArrived(object sender, ReadStringArrivedEventArgs args)
         {
             this.Invoke(GetData, args.ReadString);
-            //throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "CSV文件|*.csv";
+            openFile.FilterIndex = 0;
+            openFile.Multiselect = true;
+            openFile.ShowDialog();
+            List<string> files = new List<string>();
+            files.AddRange(openFile.FileNames);
+            Dictionary<string, string> keyValues = new Dictionary<string, string>();
+            bool ignoreAll = false;
+            foreach(string file in files)
+            {
+                Microsoft.VisualBasic.FileIO.TextFieldParser textFieldParser = new TextFieldParser(file);                
+                while (!textFieldParser.EndOfData) 
+                {
+                    textFieldParser.SetDelimiters("\t");
+                    string[] arr = textFieldParser.ReadFields();
+                    if (!keyValues.ContainsKey(arr[0]))
+                    {
+                        keyValues.Add(arr[0], arr[1]);
+                    }
+                    else
+                    {
+                        if (  ignoreAll || MessageBox.Show("发现冲突数据，是否继续？\n继续将会忽略冲突数据！！","警告",MessageBoxButtons.YesNo,MessageBoxIcon.Warning,MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
+            foreach(KeyValuePair<string,string> keyValuePair in keyValues)
+            {
+                if (!ScanData.ContainsKey(keyValuePair.Key))
+                {
+                    ScanData.Add(keyValuePair.Key, keyValuePair.Value);
+                    this.dataGridView1.Rows.Add(keyValuePair.Key, keyValuePair.Value);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog openFile = new SaveFileDialog();
+            openFile.Filter = "CSV文件|*.csv";
+            openFile.FilterIndex = 0;
+            if(DialogResult.OK == openFile.ShowDialog())
+            {
+                storeFile = openFile.FileName;
+            }
         }
     }
 }
